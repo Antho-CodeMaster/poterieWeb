ef760e95d2ece2380d26e0199462a9d2f7f71944
Nicola Filiatreault <202273631@cegepsherbrooke.qc.ca>
2024-09-13 09:51:11 -0400

    0-000 (0) | Ajout | Ajout de compte pour la connexion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,5 +25,11 @@ class DatabaseSeeder extends Seeder
             'email' => 'simondub098@gmail.com',
             'password' => 'poterie'
         ]);
+
+        User::factory()->create([
+            'name' => 'Nicola Filiatreault',
+            'email' => 'nicolafiliatreault@gmail.com',
+            'password' => 'despot'
+        ])
     }
 }
