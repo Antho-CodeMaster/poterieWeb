92cda3c3457c45d3eb86f88240461daef8c7ec63
streedrop <simondub098@gmail.com>
2024-09-16 23:13:15 -0400

    0-001 (0) | Ajout | Création des modèles, tables de migration, controllers pour tables principales

---
+++ app/Models/Mot_cle_article.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mot_cle_article extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Question_securite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question_securite extends Model
+{
+    use HasFactory;
+}
