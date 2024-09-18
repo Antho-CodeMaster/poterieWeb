da8bbcb19e1861c2b999307600af895b4ca95a5e
streedrop <simondub098@gmail.com>
2024-09-18 17:20:36 -0400

    0-002 (0) | Ajout | MotCleSeeder

--- database/seeders/MotCleSeeder.php
+++ database/seeders/MotCleSeeder.php
@@ -4,14 +4,102 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MotCleSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Ces enregistrements sont à des fins de tests et peuvent être supprimés.
      */
     public function run(): void
     {
-        //
+        DB::table('mots_cles')->insert(['mot_cle' => 'argile']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'céramique']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'potier']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'grès']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'faïence']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'porcelaine']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'terre cuite']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'glaise']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'cuisson']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'cuite']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'artisanat']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'tissage']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'vaisselle']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'vase']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'glaçure']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'néolithique']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'kaolin']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'four']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'bijou']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'archéologue']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'brique']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'faïencerie']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'fabrication']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'musée']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'vernisser']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'atelier']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'métal']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'civilisation']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'datation']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'terre']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'verrerie']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'barbotine']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'débris']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'motif']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'statuette']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'décor']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'fragment']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'outil']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'silex']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'tombe']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'ustensile']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'artisan']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'colombin']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'coquillage']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'sculpture']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'plomb']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'tesson']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'fabrique']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'fouille']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'ère']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'minoen']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'paléolithique']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'récipient']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'vannerie']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'yangshao']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'atelier de poterie']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'buse']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'calamite']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'céramiste']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'chimú']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'émail']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'engober']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'étain']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'étrusque']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'gallo-romaine']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'grossière']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'guellala']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'imperméable']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'lapita']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'lezoux']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'limon']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'marne']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'modelage']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'pisé']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'planète']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'poreuse']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'potiche']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'préhistoire']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'proche-orient']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'saladier']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'saucière']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'sigillée']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'site']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'vallauris']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'verseuse']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'vestige']);
+        DB::table('mots_cles')->insert(['mot_cle' => 'zellig']);
+
     }
 }
