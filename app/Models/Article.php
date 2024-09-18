9cc78bc7ce23c3b3fb1dccbcbcaea1e1b01585ab
Nicola Filiatreault <202273631@cegepsherbrooke.qc.ca>
2024-09-18 18:14:07 -0400

    0-000 | Ajout | liaisons bd
    liaisons concernant article, commande et transaction

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
     use HasFactory;
+    protected $table = 'article';
+    protected $primary_key = 'id_article';
+
+
+    public function artiste(){
+        return $this->belongsTo(Artiste::class,'id_artiste','id_artiste');
+    }
+    public function photo_article(){
+        return $this->hasMany(Photo_article::class,'id_article','id_article');
+    }
+    public function etat(){
+        return $this->belongsTo(Etat_article::class, 'id_etat');
+    }
+    public function collection(){
+        return $this->belongsToMany(Collection::class, 'article_collection', 'id_article', 'id_collection');
+    }
+    public function mot_cle(){
+        return $this->belongsToMany(Mot_cle::class,'mot_cle_article','id_article', 'id_mot_cle');
+    }
+    public function like(){
+        return $this->belongsToMany(User::class,'like', 'id_article','id_user');
+    }
 }
--- app/Models/Commande.php
+++ app/Models/Commande.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Commande extends Model
 {
     use HasFactory;
+    protected $table = 'commandes';
+    protected $primary_key = 'id_commande';
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class, 'id_commande');
+    }
+    public function user(){
+        return $this->belongsTo(User::class,'id_user','id_user');
+    }
+    public function ville(){
+        return $this->belongsTo(Ville::class,'id_ville', 'id_ville');
+    }
 }
