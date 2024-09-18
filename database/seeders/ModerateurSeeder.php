05f2614d9722d997ab283db74556caa5fddbf38e
streedrop <simondub098@gmail.com>
2024-09-18 17:21:55 -0400

    0-002 (0) | Ajout | ModerateurSeeder

--- database/seeders/ModerateurSeeder.php
+++ database/seeders/ModerateurSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ModerateurSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class ModerateurSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // ADMINISTRATEUR
+        DB::table('moderateurs')->insert([
+            'id_user' => 1,
+            'is_admin' => 1
+        ]);
+
+        // MODERATEUR
+        DB::table('moderateurs')->insert([
+            'id_user' => 2,
+            'is_admin' => 0
+        ]);
     }
 }
