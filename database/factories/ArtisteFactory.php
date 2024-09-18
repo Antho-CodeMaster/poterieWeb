5579398534b14376c48001c28368c6c75164328a
streedrop <simondub098@gmail.com>
2024-09-18 17:21:34 -0400

    0-002 (0) | Ajout | ArtisteFactory et ArtisteSeeder

---
+++ database/factories/ArtisteFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Artiste>
+ */
+class ArtisteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id_user' => 9, //Cette valeur ne devrait jamais être appelée, car elle doit être passée en paramètre par le seeder.
+            'id_theme' => random_int(1, 4),
+            'nom_artiste' => fake()->name(),
+            'path_photo_profil' => 'tests/pfp.jpg',
+            'is_etudiant' => random_int(0, 1),
+            'description' => fake()->sentence(random_int(5, 10), true),
+            'couleur_banniere' => '808080'
+        ];
+    }
+}
--- database/seeders/ArtisteSeeder.php
+++ database/seeders/ArtisteSeeder.php
@@ -4,14 +4,33 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Artiste;
 
 class ArtisteSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Ces enregistrements sont à des fins de tests et peuvent être supprimés.
      */
     public function run(): void
     {
-        //
+        // Compte Artiste pour ARTISTE PROFESSIONNEL
+
+        Artiste::factory()->create([
+            'id_user' => 4,
+            'is_etudiant' => 0
+        ]);
+
+        // Compte Artiste pour ARTISTE ÉTUDIANT
+
+        Artiste::factory()->create([
+            'id_user' => 5,
+            'is_etudiant' => 0
+        ]);
+
+        for ($i = 10; $i < 20; $i++) {
+            Artiste::factory()->create([
+                'id_user' => $i
+            ]);
+        }
     }
 }
