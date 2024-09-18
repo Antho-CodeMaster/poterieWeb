f5dca4c192c6374966e6b282f382a8cbcd02acc4
streedrop <simondub098@gmail.com>
2024-09-18 17:19:34 -0400

    0-002 (0) | Ajout | EtatTransactionSeeder

--- database/seeders/EtatTransactionSeeder.php
+++ database/seeders/EtatTransactionSeeder.php
@@ -4,14 +4,33 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EtatTransactionSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Ces enregistrements devront être conservés en tout temps dans la base de données.
      */
     public function run(): void
     {
-        //
+        DB::table('etats_transaction')->insert([
+            'etat' => 'Panier',
+        ]);
+
+        DB::table('etats_transaction')->insert([
+            'etat' => 'En cours',
+        ]);
+
+        DB::table('etats_transaction')->insert([
+            'etat' => 'Traité',
+        ]);
+
+        DB::table('etats_transaction')->insert([
+            'etat' => 'Livré',
+        ]);
+
+        DB::table('etats_transaction')->insert([
+            'etat' => 'Annulé',
+        ]);
     }
 }
