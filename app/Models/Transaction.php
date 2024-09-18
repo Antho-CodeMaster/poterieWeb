0c39d301584eb7ffb25a7f2745a0420bfb43450d
Nicola Filiatreault <202273631@cegepsherbrooke.qc.ca>
2024-09-18 18:25:34 -0400

    0-000 | Ajout | Lien bd
    Ajout des liens de Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -14,4 +14,19 @@ class Transaction extends Model
     public function article_non_recu(){
         return $this->hasMany(Article_non_recu::class,'id_transaction');
     }
+    public function etat_transaction(){
+        return $this->belongsTo(Etat_transaction::class, 'id_etat', 'id_etat');
+    }
+    public function article(){
+        return $this->belongsTo(Article::class, 'id_article', 'id_article');
+    }
+    public function compagnie_livraison(){
+        return $this->belongsTo(Compagnie_livraison::class, 'id_compagnie', 'id_compagnie');
+    }
+    public function photo_livraison(){
+        return $this->hasMany(Photo_livraison::class,'id_transaction', 'id_transaction');
+    }
+    public function commande(){
+        return $this->belongsTo(Commande::class, 'id_commande', 'id_commande');
+    }
 }
