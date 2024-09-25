183be5512c8c66ed5d9743ff4969f2434a1eae40
streedrop <simondub098@gmail.com>
2024-09-24 22:32:44 -0400

    0-000 (A2) | Ajout | Routes requises à l'interface admin avec usage des middlewares

---
+++ routes/admin.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Middleware\EnsureUserIsModerateur;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/admin', function() {
+    return view('admin/dashboard');
+})->name('admin')->middleware(EnsureUserIsModerateur::class);
+
+Route::get('/admin/utilisateurs', function() {
+    return view('admin/utilisateurs');
+})->name('admin-utilisateurs')->middleware(EnsureUserIsModerateur::class);
+
+Route::get('/admin/publications', function() {
+    return view('admin/publications');
+})->name('admin-publications')->middleware(EnsureUserIsModerateur::class);
+
+Route::get('/admin/signalements', function() {
+    return view('admin/signalements');
+})->name('admin-signalements')->middleware(EnsureUserIsModerateur::class);
+
+Route::get('/admin/demandes', function() {
+    return view('admin/demandes');
+})->name('admin-demandes')->middleware(EnsureUserIsModerateur::class);
+
+Route::get('/admin/articles-non-recus', function() {
+    return view('admin/articles-non-recus');
+})->name('admin-articles-non-recus')->middleware(EnsureUserIsModerateur::class);
+
+Route::get('/admin/transactions', function() {
+    return view('admin/transactions');
+})->name('admin-transactions')->middleware(EnsureUserIsModerateur::class);
