f13f386ed54129568d9b21e3c39d1e78fac09bbc
streedrop <simondub098@gmail.com>
2024-09-24 22:37:01 -0400

    S-ADT (A2) | Ajout | Transactions

---
+++ resources/views/admin/transactions.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="flex">
+        @include('admin.menu-gauche')
+        <div class="pt-20 pl-20">
+            <h1 class="text-4xl text-black">Transactions</h1>
+        </div>
+    </div>
+</x-app-layout>
