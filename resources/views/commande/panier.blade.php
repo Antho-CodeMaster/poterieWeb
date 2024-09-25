7af01351d75f1757ca1add79bab2795a6ff52738
Nicola Filiatreault <202273631@cegepsherbrooke.qc.ca>
2024-09-25 13:30:48 -0400

    01-PAN (C1) | Modification | Modif de la police pour un titre du panier

--- resources/views/commande/panier.blade.php
+++ resources/views/commande/panier.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <h1>Mon Panier</h1>
-        @if ($commande || $panier)
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">Mon Panier</h1>
+        <div>
 
-        @endif
+        </div>
 </x-app-layout>
