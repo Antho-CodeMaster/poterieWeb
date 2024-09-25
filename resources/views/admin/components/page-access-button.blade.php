0dd12a3a31054dff7ac15f3d6961ff2c621a9ae5
streedrop <simondub098@gmail.com>
2024-09-25 11:00:11 -0400

    S-ADA (A2) | Ajout | Création de boutons pour la page d'utilisateurs et les autres

---
+++ resources/views/admin/components/page-access-button.blade.php
@@ -0,0 +1,12 @@
+<button class="h-10 shadow-inner rounded border border-[#3779A9] bg-[#8DC7FB] hover:bg-[#73BCFF] mx-[10%] flex">
+    <div class="m-auto flex gap-2">
+        <svg class="w-6 h-6 my-auto" aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+            viewBox="0 0 24 24">
+            <path stroke="#3779A9" stroke-linecap="round" stroke-linejoin="round"
+                stroke-width="2"
+                d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
+        </svg>
+        <p class="text-xl text-[#3779A9]">Accéder au kiosque</p>
+    </div>
+</button>
