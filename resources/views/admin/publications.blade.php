4c7ea7be58e5102735db1ce00c3f15273cd1ae8b
Simon Dubé <simondub098@gmail.com>
2024-10-02 17:42:48 -0400

    0-000 (A2) | Correction | Uniformisation des pages administrateur

--- resources/views/admin/publications.blade.php
+++ resources/views/admin/publications.blade.php
@@ -1,8 +1,15 @@
 <x-app-layout>
-    <div class="flex">
+    <!-- Div qui divise le menu de gauche et le contenu-->
+    <div class="flex content-height">
         @include('admin.menu-gauche')
-        <div class="pt-20 pl-20 ml-[350px] mt-[48px]">
-            <h1 class="text-4xl text-black">Publications</h1>
+        <!-- Partie de droite (contenu de la page) -->
+        <div class="pt-20 px-20 h-[100%] flex flex-col">
+            <!-- Titre, nombre de résultats, filtres-->
+            <div id="header-info">
+                <h1 class="text-4xl text-black">Publications</h1>
+                <h2 class="text-2xl text-darkGrey">{{ sizeof([]) }} résultats</h2>
+            </div>
+            <!-- Reste du contenu va ici-->
         </div>
     </div>
 </x-app-layout>
