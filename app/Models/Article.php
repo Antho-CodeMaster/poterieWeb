36051b099ee29ef5a59b32951746c2f661649df0
HamidA25 <Hamid_Adelyar@hotmail.com>
2024-10-05 09:38:05 -0400

    pull origin

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -60,15 +60,19 @@ class Article extends Model
         return $this->belongsTo(Artiste::class, 'id_artiste', 'id_artiste');
     }
 
-    public function artiste(){
+    public function artiste()
+    {
         return $this->belongsTo(Artiste::class,'id_artiste','id_artiste');
     }
+
     public function photo_article(){
         return $this->hasMany(Photo_article::class,'id_article','id_article');
     }
+
     public function collection(){
         return $this->belongsToMany(Collection::class, 'article_collection', 'id_article', 'id_collection');
     }
+
     public function mot_cle(){
         return $this->belongsToMany(Mot_cle::class,'mot_cle_article','id_article', 'id_mot_cle');
     }
