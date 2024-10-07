21e8996d40c94ea37e87ef710c61aa68318d45c2
streedrop <simondub098@gmail.com>
2024-10-07 18:02:55 -0400

    0-000 (U3) | Debug | Mis l'interface de réinitialisation plus aux couleurs de l'application

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,31 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Oublié votre mot de passe? Aucun problème. Entrez votre adresse courriel, nous vérifierons votre identité et vous enverrons un lien de rénitialisation qui vous permettra d\'en choisir un nouveau.') }}
+    <div class="flex flex-col content-height justify-center">
+        <h1 class="titre text-center ">Récupérer le mot de passe</h1>
+        <div class="mx-auto w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
+            <div class="mb-4 text-sm text-gray-600">
+                {{ __('Oublié votre mot de passe? Aucun problème. Entrez votre adresse courriel, nous vérifierons votre identité et vous enverrons un lien de rénitialisation qui vous permettra d\'en choisir un nouveau.') }}
+            </div>
+
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
+            <form method="GET" action="{{ route('password.question') }}">
+                @csrf
+
+                <!-- Email Address -->
+                <div>
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                        required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-red-600 font-bold" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button>
+                        {{ __('Vérifiez mon identité!') }}
+                    </x-primary-button>
+                </div>
+            </form>
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="GET" action="{{ route('password.question') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2 text-red-600" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Vérifiez mon identité!') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
--- resources/views/auth/question-password.blade.php
+++ resources/views/auth/question-password.blade.php
@@ -1,29 +1,32 @@
-<x-app-layout>
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Veuillez répondre à cette question de sécurité afin de valider votre identité.') }}
+<x-guest-layout>
+    <div class="flex flex-col content-height justify-center">
+        <h1 class="titre text-center ">Récupérer le mot de passe</h1>
+        <div class="mx-auto w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
+            <div class="mb-4 text-sm text-gray-600">
+                {{ __('Veuillez répondre à cette question de sécurité afin de valider votre identité.') }}
+            </div>
+
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+                <input type="hidden" value={{ $email }} name="email">
+
+                <!-- Question de sécurité -->
+                <div>
+                    <x-input-label for="reponse" :value="__($question)" />
+                    <x-text-input id="reponse" class="block mt-1 w-full" type="text" name="reponse" :value="old('reponse')"
+                        required autofocus />
+                    <!-- Erreur -->
+                    <x-input-error :messages="$errors->get('reponse')" class="mt-2 text-red-600 font-bold" />
+                    <!-- Session Status (envoi réussi) -->
+                    <x-auth-session-status class="mt-2 font-bold" :status="session('status')" />
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button>
+                        {{ __('Envoyer le lien de réinitialisation') }}
+                    </x-primary-button>
+                </div>
+            </form>
         </div>
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-            <input type="hidden" value={{ $email }} name="email">
-
-            <!-- Question de sécurité -->
-            <div>
-                <x-input-label for="reponse" :value="__($question)" />
-                <x-text-input id="reponse" class="block mt-1 w-full" type="text" name="reponse" :value="old('reponse')"
-                    required autofocus />
-                <!-- Erreur -->
-                <x-input-error :messages="$errors->get('reponse')" class="mt-2 text-red-600 font-bold" />
-                <!-- Session Status (envoi réussi) -->
-                <x-auth-session-status class="mt-2 font-bold" :status="session('status')" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-primary-button>
-                    {{ __('Envoyer le lien de réinitialisation') }}
-                </x-primary-button>
-            </div>
-        </form>
     </div>
-</x-app-layout>
+</x-guest-layout>
