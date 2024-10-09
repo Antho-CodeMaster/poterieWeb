ce10f616cff7314114e058c017b0861b35dff55d
Simon Dubé <simondub098@gmail.com>
2024-10-09 16:24:33 -0400

    0-000 (0) | Debug | RécupMdp affiche maintenant l'interface de connexion si le mot de passe vient d'être reset

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -55,6 +55,9 @@ class NewPasswordController extends Controller
         // If the password was successfully reset, we will redirect the user back to
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
+        if($status == Password::PASSWORD_RESET)
+            session()->flash('succesRecupMdp', 'Votre mot de passe a été réinitialisé avec succès!');
+
         return $status == Password::PASSWORD_RESET
                     ? redirect()->route('decouverte')->with('status', __($status))
                     : back()->withInput($request->only('email'))
--- resources/views/decouverte.blade.php
+++ resources/views/decouverte.blade.php
@@ -3,11 +3,17 @@
         <h1>Découvertes</h1>
         <h2>TEST H2</h2>
         <p>TEST P</p>
-        {{-- Modal de remerciement d'avoir envoyé une demande --}}
+
         @if (Session::all())
+            {{-- Modal de remerciement d'avoir envoyé une demande --}}
             @if (Session::has('succesDemande'))
                 @include('components.devenir-artiste-succes-modal')
             @endif
+            {{-- Span qui, s'il existe, va trigger l'ouvertur du modal de connexion --}}
+            @if (Session::has('succesRecupMdp'))
+                <span id="showLoginModal" class="hidden"></span>
+            @endif
         @endif
+
     </div>
 </x-app-layout>
