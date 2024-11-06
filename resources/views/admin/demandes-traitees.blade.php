fcf53c8f04cd930d35c9da977687f03f715dcce3
streedrop <simondub098@gmail.com>
2024-11-06 10:24:39 -0500

    Meilleure vue si aucune demande n'a été traitée.

--- resources/views/admin/demandes-traitees.blade.php
+++ resources/views/admin/demandes-traitees.blade.php
@@ -79,16 +79,16 @@
                         </div>
                     @endforeach
                 @else
-                    <div class="w-full flex flex-col items-center justify-center gap-3">
+                    <div class="w-full h-full flex flex-col items-center justify-center gap-3">
                         <svg class="w-24 h-24 text-gray-800 dark:text-white" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                             viewBox="0 0 24 24">
-                            <path stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            <path stroke="#444444" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" />
                         </svg>
 
                         <h2 class="text-4xl">C'est silencieux ici...</h2>
-                        <p>Vous avez traité toutes les demandes. Vous pouvez enfin vous reposer!</p>
+                        <p>Vous n'avez traité aucune demande. Traitez-en une et ce message disparaîtra à jamais!</p>
                     </div>
                 @endif
             </div>
