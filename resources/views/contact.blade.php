5e2a7680b755a77367361af48926f0b82d6252f9
Nicola Filiatreault <202273631@cegepsherbrooke.qc.ca>
2024-11-09 23:07:59 -0500

    0-000 (I5) | Ajout | Page de contact et style de base

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,21 @@
 <x-app-layout>
-    <h1 class="titre1-dark text-center m-auto">Nous contacter</h1>
-    <div class="flex justify-center bg-slate-700">
-        <div class="w-1/3  bg-orange-500 h-3/4 relative">
-            <img src="../img/tests/photo_contact.jpg" alt="Image de poterie" class="z-0 static">
-            <div class="bg-black z-10 absolute top-5">allo</div>
+    <h1 class="titre1-dark text-center m-auto my-6">Nous contacter</h1>
+    <div class="flex justify-center w-full">
+        <div class="w-[40%] h-3/4 relative shadow-black shadow-lg m-auto">
+            <img src="../img/tests/photo_contact.jpg" alt="Image de poterie" class="z-0 static overflow-hidden">
+            <div class="bg-black z-10 absolute top-[15%] left-[70%] w-[60%] h-[60%] flex flex-col justify-evenly textGrand-light">
+                <div class="m-auto">
+                    <p class="font-semibold m-auto">Des questions?</p>
+                    <p>leofiliatreault@gmail.com</p>
+                </div>
+                <div class="m-auto">
+                    <p class="font-semibold m-auto">Besoin d'aide? (moyenant frais)</p>
+                    <p>HamidA24@cegepsherbrooke.qc.ca</p>
+                </div>
+            </div>
         </div>
 
-        <div class="w-1/2">Informations de contacte cute</div>
+        <div class="w-1/3 m-auto">Informations de contacte cute</div>
     </div>
 
 </x-app-layout>
