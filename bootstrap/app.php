1448729c3ce02e04d24802a436943315c3c6f387
streedrop <simondub098@gmail.com>
2024-11-20 21:55:29 -0500

    IMPORTANT: dé-commenter ce code une fois que le site sera en ligne.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Exceptions\Handler;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,5 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+/*         $exceptions->render(function (Throwable $exception) {
+            return response()->view('errors.general', ['exception' => $exception], 500);
+        }); */
     })->create();
